788709019b6c98539d1a1d02cefdf10de38579c1
Mirelli Safiri Navarro <mirelli.navarro@gmail.com>
2012-08-29 22:44:05 -0300

    adicionando tela visual principal

--- Hotel/src/jhotel/com/Checkin.java
+++ Hotel/src/jhotel/com/Checkin.java
@@ -1,14 +1,58 @@
 package jhotel.com;
 
-public class Checkin {
-   
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Checkin extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;  //pra que serve isso?
+	private JButton butao;	
+	private JPanel painel;
+	
 	public Checkin(String cpf){
-		if (ClienteTemReserva(cpf) == true){
+		
+		
+		painel=new JPanel();
+		painel.setLayout(new FlowLayout());	
+		butao=new JButton("NOVA");
+		butao.addActionListener(new Sair());
+		painel.add(butao);
+		
+		this.add(painel);
+		this.setSize(300,200);
+		this.setResizable(false);
+		this.setVisible(true);			
+		
+		
+		if ((ClienteTemReserva(cpf) == true) || (ClienteTemCadastro(cpf) == true)){
 			ReservaQuarto(cpf);
 		}else
 		{
 			CadastraCliente(cpf);
 		}
+	}
+	
+	private boolean ClienteTemCadastro(String cpf) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private class Sair implements ActionListener{	
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			
+			new Jhotel();
+		}			
 	}
 	
 	
@@ -28,3 +72,5 @@ private boolean ClienteTemReserva(String cpf){
   
   
 }
+
+
